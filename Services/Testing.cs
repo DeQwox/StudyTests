4d5bef47a8f95129f9322188c470523e433e77b4
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2025-09-27 22:01:50 +0300

    added missing files

---
+++ Services/Testing.cs
@@ -0,0 +1,63 @@
+using System.Data;
+using System.Threading.Tasks;
+using Repositories;
+using StudyTests.Models.Entities;
+
+namespace Services;
+
+public class Testing
+{
+    private readonly ITestingRepository _testingRepository;
+    private Test _test;
+    private int[] _order = [];
+    private int _current = 0;
+    private List<Question> _questions = [];
+    private int[] _answers = [];
+
+    private int _studentId;
+    private int _testId;
+
+    public Testing(int testId, int studentId, ITestingRepository testingRepository)
+    {
+        _testId = testId;
+        _studentId = studentId;
+
+        _testingRepository = testingRepository;
+        _test = _testingRepository.GetTestById(testId) ?? throw new Exception("Test doesn't exist");
+
+        _questions = _testingRepository.GetTestQuestionList(testId).ToList();
+        _order = Enumerable.Range(0, _questions.Count).ToArray();
+        new Random().Shuffle(_order);
+        
+        
+        _answers = new int[_questions.Count];
+    }
+
+    public int GetQuestionsCount()
+    {
+        return _questions.Count;
+    }
+
+    public Question GetQuestion()
+    {
+        return _questions[_order[_current++]];
+    }
+
+    public void Answer(int answer)
+    {
+        _answers[_order[_current - 1]] = answer;
+    }
+    
+    public async Task<PassedTest> GetResult()
+    {
+        return new PassedTest()
+        {
+            Student = await _testingRepository.GetStudentByIdAsync(_studentId) ?? throw new Exception("Student doesn't exist"),
+            Test    = await _testingRepository.GetTestByIdAsync(_testId) ?? throw new Exception("Test doesn't exist"),
+            Answers = _answers.Select(i => i.ToString()).ToList(),
+            Score = _questions.Zip(_answers)
+                        .Select(i => (i.First.CorrectAnswerIndex == i.Second) ? i.First.Score : 0)
+                        .Sum()
+        };
+    }
+}
