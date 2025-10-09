4471739dbf7919f42f8a4f63c7ecb9b1f0af515c
Artem <artemgrisenko039@gmail.com>
2025-10-09 16:33:28 +0300

    Add unique index for PhoneNumber and update User model properties to use null! for non-nullable fields

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -5,22 +5,32 @@ namespace StudyTests.Models.Entities;
 
 [Index(nameof(Login), IsUnique = true)]
 [Index(nameof(Email), IsUnique = true)]
-public class User: Entity
+[Index(nameof(PhoneNumber), IsUnique = true)]
+public class User : Entity
 {
     [Required]
     [MaxLength(200)]
-    public string Login { get; set; } = string.Empty;
+    public string Login { get; set; } = null!;
 
     [Required]
     [MaxLength(100)]
-    public string FirstName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = null!;
     [Required]
     [MaxLength(100)]
-    public string LastName { get; set; } = string.Empty;
+    public string LastName { get; set; } = null!;
 
     [Required]
     [EmailAddress]
     [MaxLength(255)]
-    public string Email { get; set; } = string.Empty;
+    public string Email { get; set; } = null!;
 
+    [Required]
+    [MaxLength(20)]
+    [Phone]
+    public string PhoneNumber { get; set; } = null!;
+
+    [Required]
+    [MaxLength(255)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
 }
