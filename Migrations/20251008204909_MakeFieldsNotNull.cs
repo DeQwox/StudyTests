9074c15c6d85daaa0f485236bb37da83497336cb
Artem <artemgrisenko039@gmail.com>
2025-10-09 16:30:57 +0300

    Add migrations

---
+++ Migrations/20251008204909_MakeFieldsNotNull.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyTests.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeFieldsNotNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PassedTests_Students_StudentId",
+                table: "PassedTests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tests_Teachers_TeacherID",
+                table: "Tests");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Teachers",
+                table: "Teachers");
+
+            migrationBuilder.RenameTable(
+                name: "Teachers",
+                newName: "Users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Teachers_Login",
+                table: "Users",
+                newName: "IX_Users_Login");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Teachers_Email",
+                table: "Users",
+                newName: "IX_Users_Email");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                type: "TEXT",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PassedTests_Users_StudentId",
+                table: "PassedTests",
+                column: "StudentId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tests_Users_TeacherID",
+                table: "Tests",
+                column: "TeacherID",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PassedTests_Users_StudentId",
+                table: "PassedTests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tests_Users_TeacherID",
+                table: "Tests");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Teachers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Login",
+                table: "Teachers",
+                newName: "IX_Teachers_Login");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Email",
+                table: "Teachers",
+                newName: "IX_Teachers_Email");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Teachers",
+                table: "Teachers",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Login = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Email",
+                table: "Students",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Login",
+                table: "Students",
+                column: "Login",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PassedTests_Students_StudentId",
+                table: "PassedTests",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tests_Teachers_TeacherID",
+                table: "Tests",
+                column: "TeacherID",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
