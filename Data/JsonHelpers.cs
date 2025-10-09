18bdd98c068e84bc4f3538fb0c7ff9e69946deec
Artem <artemgrisenko039@gmail.com>
2025-10-09 16:33:52 +0300

    Add JsonHelpers class for serializing and deserializing answer lists

---
+++ Data/JsonHelpers.cs
@@ -0,0 +1,16 @@
+using System.Text.Json;
+
+namespace Data;
+
+public static class JsonHelpers
+{
+    public static string SerializeAnswers(System.Collections.Generic.List<string> answers)
+    {
+        return JsonSerializer.Serialize(answers);
+    }
+
+    public static System.Collections.Generic.List<string> DeserializeAnswers(string json)
+    {
+        return JsonSerializer.Deserialize<System.Collections.Generic.List<string>>(json) ?? new System.Collections.Generic.List<string>();
+    }
+}
